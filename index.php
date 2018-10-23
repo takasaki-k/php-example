2f15414670ac7f60f36ce335530f8903edcfc81f
takasaki-k <44356412+takasaki-k@users.noreply.github.com>
2018-10-23 11:16:22 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
   echo "<h1>Hello world!</h1>\n"; 
   echo "hostname: " . gethostname() . "<BR>\n";
-  echo "IP-Addr : " . $_SERVER['SERVER_ADDR'] . "\n"; ;?>
-  echo "<h3>TODAYS redhat-handson!</h3>\n"; 
+  echo "IP-Addr : " . $_SERVER['SERVER_ADDR'] . "\n"; 
+  echo "<h3>TODAYS redhat-handson!</h3>\n"; ;?>
