72b00ad83f5c3fe32b9e0428619a0484b7c58524
takasaki-k <44356412+takasaki-k@users.noreply.github.com>
2018-10-23 11:45:10 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
-  echo "<h1>goodbye world!</h1>\n"; 
+  echo "<h1>goodbye world! </h1>\n"; 
   echo "hostname: " . gethostname() . "<BR>\n";
   echo "IP-Addr : " . $_SERVER['SERVER_ADDR'] . "\n"; ;?>
